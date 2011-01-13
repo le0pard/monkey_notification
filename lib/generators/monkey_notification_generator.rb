04b64c136d81ae2ffffbd5528aa3e26e3878df7e
Alexey <leopard.not.a@gmail.com>
2011-01-13 23:31:36 +0200

    Fix generator

--- lib/generators/monkey_notification_generator.rb
+++ lib/generators/monkey_notification_generator.rb
@@ -34,29 +34,5 @@ require 'monkey_notification/capistrano'
   def generate_initializer
     template 'initializer.rb', 'config/initializers/monkey_notification.rb'
   end
-
-
-  def manifest
-    if !options[:api_url]
-      puts "Must pass --api-url"
-      exit
-    end
-    record do |m|
-      m.directory 'lib/tasks'
-      m.file 'monkey_notification_tasks.rake', 'lib/tasks/monkey_notification_tasks.rake'
-      if ['config/deploy.rb', 'Capfile'].all? { |file| File.exists?(file) }
-        m.append_to 'config/deploy.rb', capistrano_hook
-      end
-      if api_key_expression
-        m.template 'initializer.rb', 'config/initializers/monkey_notification.rb',
-            :assigns => {:api_url => api_url_expression}
-      end
-      #m.rake "hoptoad:test --trace", :generate_only => true
-    end
-  end
-  
-  def api_url_expression
-    "'#{options[:api_url]}'"
-  end
   
 end
